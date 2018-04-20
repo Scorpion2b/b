f7446f0bd1903b0c1408e8fa0931ac4cba1fb83c
Scorpion2b <38207476+Scorpion2b@users.noreply.github.com>
2018-04-20 13:47:14 +0200

    Update badwords.js

--- badwords.js
+++ badwords.js
@@ -1,5 +1,6 @@
 function badword() {
 	const swearWords = ["PD", "POULOULOU", "BUMBUMTAM", "SALE ARABE", "SALE ARABE", "SALE ARABE", "CONNARD", "BATARD", "FOUTRE", "VA TE FAIRE", "SUCE", "BITE", "FILS DE PUTE", "ENCULE", "SALE NEGRE","SALE NOIR","SALE JUIF","NAZI","SALOPE","SALOP","SALOPARD","TCHOIN","TEPU","PUTE","CATIN","MANGE TES MORT","NIQUE"];
+	return(swearWords);
 }
 
 function badword_punition(){
