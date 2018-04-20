06ce602f70079be9755f5e68d317ca9badb6ebef
Scorpion2b <38207476+Scorpion2b@users.noreply.github.com>
2018-04-20 14:08:36 +0200

    Update badwords.js

--- badwords.js
+++ badwords.js
@@ -7,4 +7,31 @@ function badword_punition(){
 
 	message.reply("Vous n'ête pas autorisé à dire ceci !");
   	message.delete();
+
+  	var vérif = message.author.id;
+  	for(i=0 ; i < juge ; i++)
+  	{
+  		if (casier[i] === vérif) 
+  		{
+  			amende[i]++;
+  			i = juge;
+  		}
+  		else
+  		{
+  			juge++;
+  			casier[juge] = vérif;
+  			i = juge;
+  		}
+  	}
+
+  	for(i=0 ; i < juge ; i++)
+  	{
+  		if (amende[i] > 3) 
+  		{
+			message.send('coucou tu va prendre cher !');
+  			i = juge;
+  		}
+  		else
+  		{}
+  	}
 }
