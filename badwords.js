ac3f0b0d07bdc27231008f0106689101a17fcc8e
Scorpion2b <38207476+Scorpion2b@users.noreply.github.com>
2018-04-20 13:38:03 +0200

    Add files via upload

---
+++ badwords.js
@@ -0,0 +1,18 @@
+function badword() {
+	const swearWords = ["PD", "POULOULOU", "BUMBUMTAM", "SALE ARABE", "SALE ARABE", "SALE ARABE", "CONNARD", "BATARD", "FOUTRE", "VA TE FAIRE", "SUCE", "BITE", "FILS DE PUTE", "ENCULE", "SALE NEGRE","SALE NOIR","SALE JUIF","NAZI","SALOPE","SALOP","SALOPARD","TCHOIN","TEPU","PUTE","CATIN","MANGE TES MORT","NIQUE"];
+}
+
+function badword_punition(){
+
+	message.reply("Vous n'ête pas autorisé à dire ceci !");
+  	message.delete();
+  	if (!userData[sender.id]) userData[sender.id] = {
+		messageSent: 0
+  	}
+
+  	userData[sender.id].messagesSent++;
+  	fs.writeFile('userData.json' , JSON.stringify(userData), (err) =>{
+  		if (err) console.error(err);
+  	});
+
+}
